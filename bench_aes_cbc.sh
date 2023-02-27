54258c0c91fb828588cb91ff0ba52fa904ac117d
Robin Hundt <24554122+robinhundt@users.noreply.github.com>
2023-02-27 19:11:48 +0100

    Update bench scripts

--- bench_aes_cbc.sh
+++ bench_aes_cbc.sh
@@ -9,5 +9,5 @@ for in_blocks in {1,2,8,16,32,64,128,256,512,1024};do
     heaptrack="$HOME/heaptrack/build/bin/heaptrack -o heaptrack_MP_SPDZ_$in_blocks"
   fi
   ./compile.py -B 64 aescbc_circuit $in_blocks
-   $heaptrack ./semi-bin-party.x $id aescbc_circuit-$in_blocks -F -pn 12353 -h 130.83.125.166 > MP_SPDZ_block_$in_blocks.log
+   $heaptrack ./semi-bin-party.x $id aescbc_circuit-$in_blocks -F -pn 12353 -h 130.83.125.166 2> MP_SPDZ_block_$in_blocks.log
 done
